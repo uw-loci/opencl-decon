75a6ef9a2315e8f168df8ac49fcb1d2ede611651
Rick Lentz <rlentz@gmail.com>
2011-01-13 15:47:02 +0000

    Restructured libs and code to reflect CUDA 3.2 and Jogamp's signed build from Dec 12, 2010 found at http://jogamp.org/deployment/webstart/

--- src/org/imagejdev/api/WriteNativeLibsToJAVALIBRARYPATH.java
+++ src/org/imagejdev/api/WriteNativeLibsToJAVALIBRARYPATH.java
@@ -1,30 +1,47 @@
 package org.imagejdev.api;
 
 /*
- * This class
+ * This class is helpful for extracting OS Specific dynamic libs from jar files
+ * to $TEMP $TMPDIR $TMP
  */
 public class WriteNativeLibsToJAVALIBRARYPATH 
 {
 	// Given an OS, copy the native libs to the build path
-	public enum OSCHOICE 
-	{
-		APPLE, LINUX64, LINUX32, WINDOWS64, WINDOWS32
-	};
+	public enum OSCHOICE { APPLE, LINUX64, LINUX32, WINDOWS64, WINDOWS32 };
 
-	public static void copyNativeLibs(OSCHOICE os) 
-	{
+	/*
+	 * For each jar / os specific file extension get the input stream 
+	 * Iterate over the jar entries 
+	 * Get input stream Write the file to the path
+	 */
+	public static void copyNativeLibs( OSCHOICE os ) {
+
+		switch ( os ) {
+		case APPLE: {
+			
+		}
+
+		case LINUX64: {
+
+		}
+
+		case LINUX32: {
+
+		}
+
+		case WINDOWS32: {
+
+		}
+
+		case WINDOWS64: {
+
+		}
+
+		default: {
+			break;
+		}
 		
-		switch (os) {
-			case APPLE: 
-			{
-
-			}
-
-			default: 
-			{
-				break;
-			}
 		} // end switch
-		
 	}
+	
 }
